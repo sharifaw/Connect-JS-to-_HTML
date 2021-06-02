a73fa50e130b2d5cbee861f68ebbaf8b7382d5b4
shreefaw <shreefaw@gmail.com>
2021-06-02 20:54:16 +0300

    pure

--- ChangeElementStyle/script.js
+++ ChangeElementStyle/script.js
@@ -1,13 +1,13 @@
 const select = document.getElementsByTagName('select')[0];
 select.addEventListener('change',checkOptions);
-submit.classList.remove('disabled');
+
 submit.addEventListener('click',changeColor);
 
 
 function checkOptions() {
-    for (var i = 0; i < select.length; i++) {
-        if (select.options[i].selected === true){
-                if (select.options[i].value != "")
+    for (var index = 0; index < select.length; index++) {
+        if (select.options[index].selected === true){
+                if (select.options[index].value != "")
                 {
                     
                     submit.classList.remove('disabled');
@@ -25,23 +25,23 @@ function checkOptions() {
 function changeColor(event)
 {
     event.preventDefault();
-    for (var i = 0; i < select.length; i++)
+    for (var index = 0; index < select.length; index++)
     {
-        if (select.options[i].selected === true)
+        if (select.options[index].selected === true)
         {
-            if(select.options[i].value === "red")
+            if(select.options[index].value === "red")
             {
             document.body.classList.add('red');
             }
-            if(select.options[i].value === "green")
+            if(select.options[index].value === "green")
             {
             document.body.classList.add('green');
             }
-            if(select.options[i].value === "blue")
+            if(select.options[index].value === "blue")
             {
             document.body.classList.add('blue');
             }
-            if(select.options[i].value === "purple")
+            if(select.options[index].value === "purple")
             {
             document.body.classList.add('purple');
             }
--- GetSetValues/script.js
+++ GetSetValues/script.js
@@ -1,32 +1,33 @@
 value1.addEventListener("input",checkNumber);
 value2.addEventListener("input",checkNumber);
-if(value1.value === ""){
-    value1.value=0;
-}
-if(value2.value === ""){
-    value2.value=0;
+
+
+function zeroValue(input){
+    if(input.value === ""){
+        input.value=0;
+    }
 }
 
-function checkNumber(){
-if(isNaN(value1.value) || isNaN(value2.value)){
+function checkNumber(input){
+    
+if(isNaN(input.target.value)){
     alert("Value X must be a number");
 }
 }
 
 function assignment(){
-    
-    for (var i = 0; i <select.length; i++){
-        if(select.options[i].selected == true){
-            if(select.options[i].value == "+"){
+    for (var index = 0; index <select.length; index++){
+        if(select.options[index].selected == true){
+            if(select.options[index].value == "+"){
                 return result.value = +(value1.value) + +(value2.value) ;
             }
-            if(select.options[i].value == "/"){
+            if(select.options[index].value == "/"){
                 return result.value = +(value1.value) / +(value2.value) ;
             }
-            if(select.options[i].value == "-"){
+            if(select.options[index].value == "-"){
                 return result.value = +(value1.value) - +(value2.value) ;
             }
-            if(select.options[i].value == "*"){
+            if(select.options[index].value == "*"){
                 return result.value = +(value1.value) * +(value2.value) ;
             }
         }
@@ -35,7 +36,8 @@ function assignment(){
 }
  
 button.onclick = function(){
-
+    zeroValue(value1);
+    zeroValue(value2);
      assignment();
 }
 
